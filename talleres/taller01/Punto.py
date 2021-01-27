7086d133b0bda8395061e1ab2dd580e9a0d2d508
imontoyah <77788437+imontoyah@users.noreply.github.com>
2021-01-27 08:32:37 -0500

    Add files via upload

---
+++ talleres/taller01/Punto.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 27 07:51:19 2021
+
+@author: LENOVO
+"""
+import math
+
+class Punto2D():
+    """Representacion de punto en 2 dimenciones"""
+
+ 
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+ 
+
+    def get_x(self):
+        return self.x
+        
+    def get_y(self):
+        return self.y
+      
+    def radio_polar(self):
+       return math.sqrt((self.x*self.x)+(self.y*self.y))
+
+ 
+
+    def angulo_polar(self):
+        return math.atan(self.y/self.x)
+
+ 
+
+    
+    def dist_euclidiana(self, other):
+        dx = self.x - other.get_x()
+        dy = self.y- other.get_y()
+        return math.sqrt((dx*dx)+(dy*dy))
+    
+class main():
+    punto1 = Punto2D(10,20)
+    print ("El valor de x es: "+ str(punto1.get_x()))
+    print("El radio polar es de: "+str(punto1.radio_polar()))
+    print("El angulo polar es de: "+ str(punto1.angulo_polar()))
+    punto2 = Punto2D(0,0)
+    print("La distancia entre los dos puntos es de: "+ str(punto1.dist_euclidiana(punto2)))
